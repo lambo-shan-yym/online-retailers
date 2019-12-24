eabd8417ac7cf0338379b42ad8fa4b53a04838e6
lambo <786285393@qq.com>
2019-12-24 11:57:56 +0800

    项目初始化

---
+++ src/main/java/com/lambo/onlineretailers/OnlineRetailersApplication.java
@@ -0,0 +1,13 @@
+package com.lambo.onlineretailers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRetailersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambo/onlineretailers/OnlineRetailersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambo.onlineretailers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRetailersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
